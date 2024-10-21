8674f9c2595c8e861420dde0bb2a35e5df43918d
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-10-21 09:49:33 -0500

    defined the sales category query

---
+++ intermediate/sales-category.sql
@@ -0,0 +1,9 @@
+select
+    name,
+    salary
+
+    case
+        when salary < 6000 then 'low'
+        where salary between 6000 and 7000 then 'medium'
+        else 'high'
+    end as salary_category
